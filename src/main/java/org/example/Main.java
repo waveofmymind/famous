5ade0a902805e26947c7b7754d34bc3955896837
waveofmymind <sjun0913@gmail.com>
2023-03-02 09:23:40 +0900

    [FIX] 2단계 클리어

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("== 명언 앱 ==");
         while (true) {
-            System.out.println("== 명언 앱 ==");
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
             if (cmd.equals("종료")) {
                 break;
+            }
+            if (cmd.equals("등록")) {
+                System.out.printf("명언 : ");
+                String say = sc.nextLine();
+                System.out.printf("작가 : ");
+                String author = sc.nextLine();
             }
         }
     }
